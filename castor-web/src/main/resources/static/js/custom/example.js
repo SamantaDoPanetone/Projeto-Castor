f5e4029692725c33cb363ce401f0f7e84ce5409c
Fabiano Nascimento Leite <fabianofnl@gmail.com>
2026-02-11 11:38:32 -0300

    issue-4 - Organizar código front
    Implementado o uso de fragments
    
    Signed-off-by: Fabiano Nascimento Leite <fabianofnl@gmail.com>

--- castor-web/src/main/resources/static/js/custom/example.js
+++ castor-web/src/main/resources/static/js/custom/example.js
@@ -23,10 +23,45 @@ document.addEventListener('alpine:init', () => {
             this.pessoaId = Number(dataset.id)
             this.pessoaName = dataset.name
         }
+
     }))
 })
 
-setTimeout(() => {
-    const alert = document.querySelector('.alert')
-    if (alert) alert.remove()
-}, 4000)
+document.body.addEventListener('htmx:afterSwap', function (event) {
+
+    // Fecha modal de ADD / EDIT
+    const pessoaModalEl = document.getElementById('pessoaModal')
+    if (pessoaModalEl) {
+        const modal = bootstrap.Modal.getInstance(pessoaModalEl)
+        if (modal) modal.hide()
+    }
+
+    // Fecha modal de DELETE
+    const deleteModalEl = document.getElementById('deletePessoaModal')
+    if (deleteModalEl) {
+        const modal = bootstrap.Modal.getInstance(deleteModalEl)
+        if (modal) modal.hide()
+    }
+
+    // Toast
+    const toastElements = document.querySelectorAll('#pessoas-content .toast')
+        toastElements.forEach(function (toastEl) {
+            // Move para o container fixo
+            document.getElementById('toast-container').appendChild(toastEl)
+            const toast = new bootstrap.Toast(toastEl)
+            toast.show()
+        })
+})
+
+//setTimeout(() => {
+//    const alert = document.querySelector('.alert')
+//    if (alert) alert.remove()
+//}, 4000)
+
+function showToast() {
+    const toastElList = document.querySelectorAll('.toast')
+    toastElList.forEach(toastEl => {
+        const toast = new bootstrap.Toast(toastEl)
+        toast.show()
+    })
+}
